e459357230e16e03c6ae7f4882613308c8e6fbb4
gmason <gmason@msu.edu>
2012-07-23 11:24:31 -0500

    move to /usr/local/sbin

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,15 @@
 class zfs_auto_snapshot {
   include zfs_auto_snapshot::cron
-# Below needed only if we're putting it in /usr/local.
-  /*
-  file { '/usr/local/zfs-auto-snapshot' :
+# Installs to /usr/local/sbin.
+  file { '/usr/local' :
     ensure => directory,
     owner => 'root', group => 'root', mode => 0755,
   }
-  file { '/usr/local/zfs-auto-snapshot/sbin' :
+  file { '/usr/local/sbin' :
     ensure => directory,
     owner => 'root', group => 'root', mode => 0755,
   }
-  */
-# Going with /usr/sbin instead as OmniOS is quite minimal.
-# Chance of a collision is quite low.
-  file { '/usr/sbin/zfs-auto-snapshot' :
+  file { '/usr/local/sbin/zfs-auto-snapshot' :
     ensure => present,
     source => "puppet:///modules/${module_name}/zfs-auto-snapshot.pl",
     owner => 'root', group => 'root', mode => 0755,
