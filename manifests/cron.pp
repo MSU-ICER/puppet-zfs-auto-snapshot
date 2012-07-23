c7c15d85af55a65eb862ca7abfcd103a61e2229e
gmason <gmason@msu.edu>
2012-07-23 11:50:05 -0500

    move config data to params subclass

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,18 +1,17 @@
 class zfs_auto_snapshot::cron {
-  $fsname = [ 'localpool' ]
   cron { 'zfssnap_hourly': 
-    command  => "/usr/sbin/zfs-auto-snapshot --syslog --label hourly --keep 24 --recursive $fsname",
+    command  => "/usr/sbin/zfs-auto-snapshot --syslog --label hourly --keep $zfs_auto_snapshot::params::hourly_snaps --recursive $zfs_auto_snapshot::params::$fsname",
     user     => 'root',
     minute   => 0,
   }
   cron { 'zfssnap_daily': 
-    command  => "/usr/sbin/zfs-auto-snapshot --syslog --label daily --keep 7 --recursive $fsname",
+    command  => "/usr/sbin/zfs-auto-snapshot --syslog --label daily --keep $zfs_auto_snapshot::params::daily_snaps --recursive $zfs_auto_snapshot::params::$fsname",
     user     => 'root',
     minute   => 0,
     hour     => 0,
   }
   cron { 'zfssnap_weekly': 
-    command  => "/usr/sbin/zfs-auto-snapshot --syslog --label weekly --keep 4 --recursive $fsname",
+    command  => "/usr/sbin/zfs-auto-snapshot --syslog --label weekly --keep $zfs_auto_snapshot::params::weekly_snaps --recursive $zfs_auto_snapshot::params::$fsname",
     user     => 'root',
     minute   => 0,
     hour     => 0,
