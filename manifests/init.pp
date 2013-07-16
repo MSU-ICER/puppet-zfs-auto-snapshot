654d91592c5a4c16d537266f2dcb07af919f4254
Andrew Rice <acr31@cam.ac.uk>
2013-07-16 13:59:21 +0100

    Removed File classes for /usr/local and /usr/local/sbin
    These directories can be assumed to exist based on the Filesystem Hierachy Standard.  Its wrong to try and 'ensure' them here because a) they might conflict with people (also incorrectly) 'ensuring' them in other modules/manifests b) in our configuration they create a circular depedency loop between stages

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,9 @@
 class zfs_auto_snapshot($pool_names) {
   include zfs_auto_snapshot::cron
-# Installs to /usr/local/sbin.
-  file { '/usr/local' :
-    ensure => directory,
-    owner => 'root', group => 'root', mode => 0755,
   }
-  file { '/usr/local/sbin' :
-    ensure => directory,
-    owner => 'root', group => 'root', mode => 0755,
-  }
+
+  # Installs to /usr/local/sbin.which we can assume exists as per the Filesystem Hierachy Standard
+
   file { '/usr/local/sbin/zfs-auto-snapshot' :
     ensure => present,
     source => "puppet:///modules/${module_name}/zfs-auto-snapshot.pl",
