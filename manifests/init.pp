03557e3385573f8f0c53fd53c26e2572d48d5682
Andrew Rice <acr31@cam.ac.uk>
2013-07-16 14:00:12 +0100

    Refactored the use of parameters
    The puppet guide offers a best-practice pattern for the use of parameters for classes:
    http://docs.puppetlabs.com/guides/parameterized_classes.html
    I've refactored to follow this.  It basically involves inheriting from your params class rather than using dynamic variable lookup.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,16 @@
-class zfs_auto_snapshot($pool_names) {
-  include zfs_auto_snapshot::cron
+class zfs_auto_snapshot(
+  $hourly_snaps = $zfs_auto_snapshot::params::hourly_snaps,
+  $daily_snaps  = $zfs_auto_snapshot::params::daily_snaps,
+  $weekly_snaps = $zfs_auto_snapshot::params::weekly_snaps,
+  $pool_names   = $zfs_auto_snapshot::params::pool_names,
+)
+inherits zfs_auto_snapshot::params {
+
+  class {'zfs_auto_snapshot::cron':
+    hourly_snaps => $hourly_snaps,
+    daily_snaps  => $daily_snaps,
+    weekly_snaps => $weekly_snaps,
+    pool_names   => $pool_names,
   }
 
   # Installs to /usr/local/sbin.which we can assume exists as per the Filesystem Hierachy Standard
